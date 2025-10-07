68c8ce8255e26515412d787b36c1e373d76553d3
Reshma-K-S <reshmaks3110@gmail.com>
2025-10-07 12:28:40 +0530

    Adjust quizzes initial migration dependency to local courses state

--- quizzes/migrations/0001_initial.py
+++ quizzes/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('courses', '0004_category_slug_alter_category_name'),
+        ('courses', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
